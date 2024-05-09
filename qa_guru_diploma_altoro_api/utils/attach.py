312601f0cf3d4cca35566540800a4be14a8d6959
Irina.Voron <yarutich.irina@gmail.com>
2024-05-09 14:41:54 +0300

    Add attach.py

---
+++ qa_guru_diploma_altoro_api/utils/attach.py
@@ -0,0 +1,29 @@
+import os
+
+import allure
+from selene import browser
+from allure_commons.types import AttachmentType
+
+
+def add_screenshot():
+    png = browser.driver.get_screenshot_as_png()
+    allure.attach(body=png, name='screenshot', attachment_type=AttachmentType.PNG, extension='.png')
+
+
+def add_logs():
+    logs = ''.join(f'{text}\n' for text in browser.driver.get_log(log_type='browser'))
+    allure.attach(body=logs, name='logs', attachment_type=AttachmentType.TEXT, extension='.log')
+
+
+def add_html():
+    html = browser.driver.page_source
+    allure.attach(body=html, name='html', attachment_type=AttachmentType.HTML, extension='.html')
+
+
+def add_video():
+    selenoid_url = os.getenv('SELENOID_URL')
+    video_url = f'https://{selenoid_url}/video/{browser.driver.session_id}.mp4'
+    html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
+           + video_url \
+           + "' type='video/mp4'></video></body></html>"
+    allure.attach(body=html, name='video', attachment_type=AttachmentType.HTML, extension='.html')
--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -1,18 +1,15 @@
 import os
-
-import pytest
-from selene import browser
 from dotenv import load_dotenv
-from selene import browser, have
-import requests
+from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import pytest
 
+from qa_guru_diploma_altoro_api.utils import attach
+
 load_dotenv()
 base_url = os.getenv('BASE_URL')
-# username = os.getenv('USER_NAME')
-# password = os.getenv('PASSWORD')
+
 selenoid_url = os.getenv('SELENOID_URL')
 selenoid_login = os.getenv('SELENOID_LOGIN')
 selenoid_password = os.getenv('SELENOID_PASSWORD')
@@ -40,5 +37,10 @@ def browser_management():
     browser.config.driver = driver
 
     yield
+
+    attach.add_screenshot()
+    attach.add_html()
+    attach.add_logs()
+    attach.add_video()
 
     browser.quit()
